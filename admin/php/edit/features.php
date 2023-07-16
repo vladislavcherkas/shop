300d14c199d39ddd20b4ed62b95072d8d97ee773
Vladislav Cherkas <vladislavcherkas2005a@gmail.com>
2023-07-16 22:32:41 +0300

    Add functional

--- admin/php/edit/features.php
+++ admin/php/edit/features.php
@@ -1,3 +1,21 @@
 <?php
-echo "true";
+require_once($_SERVER["DOCUMENT_ROOT"] . "/php/products/editor-products.php");
+$type = $_POST["type"];
+$product_id = $_POST["productId"];
+if ($type === "get") {
+    echo "true";
+}
+if ($type === "all") {
+    echo "true";
+}
+if ($type === "single") {
+    echo "true";
+}
+if ($type === "add") {
+    EditorProducts::addFeature($product_id);
+    echo "true";
+}
+if ($type === "remain") {
+    echo "true";
+}
 ?>
